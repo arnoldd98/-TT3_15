fb7d6e36569d9847aa2f32c9038c94e96f7c527e
Haritha Divakaran <haritha.divakar@gmail.com>
2022-01-20 11:19:55 +0800

    template for frontend

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Container>Home Page</Container>
+    </div>
+  );
+};
+
+export default HomePage;

a4bd7302c85401fb985f87f08729697b0ac403ab
Abysscal <calvinhe156@gmail.com>
2022-01-20 16:15:19 +0800

    code cleaning

--- backend/api.py
+++ backend/api.py
@@ -137,49 +137,5 @@ class User(db.Model):
         self.city = city
         self.country = country
 
-# app = Flask(__name__)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/socialmedia'
-
-# db = SQLAlchemy(app)
-
-# class LikedPost(db.model):
-#     user_id = db.Column('User_ID',db.Integer,primary_key = True,required= True)
-#     post_id = db.Column('Post_ID',db.Integer,primary_key = True,required = True)
-
-# class Post(db.model):
-#     post_id = db.Column('Post_ID',db.Integer,primary_key = True)
-#     post_title = db.Column('Post_Title',db.String(50))
-#     post_description = db.Column('Post_Description',db.String(200),nullable=True)
-#     post_image = db.Column('Post_image',db.String(300),nullable=True)
-
-# class PostComment(db.model):
-#     comment_id = db.Column('Comment_ID',db.Integer,primary_key = True)
-#     user_id = db.Column('User_ID',db.Integer)
-#     post_id = db.Column('Post_ID',db.Integer)
-#     # consensus to set 280 char limit
-#     comment = db.Column('Comment',db.String(280),nullable=True)
-
-# class User(db.model):
-#     user_id = db.Column('User_ID',db.Integer,primary_key = True)
-#     password = db.Column('Password',db.String(16))
-#     name = db.Column('Name',db.String(50))
-#     age = db.Column('Age',db.Integer,nullable=True)
-#     # date will be DDMMYYYY
-#     birthday = db.Column('Birthday',db.String(8),nullable=True)
-#     email = db.Column('Email',db.String(50),nullable=True)
-#     phone = db.Column('Phone',db.String(50),nullable=True)
-#     city = db.Column('City',db.String(50),nullable=True)
-#     country = db.Column('Country',db.String(50),nullable=True)
-
-# @app.route('/user', method=['GET'])
-# def get_all_users():
-#     return ''
-
-# @app.route('/user/<user_id>', method = ['GET'])
-# # do authen here
-# def auth():
-#     return ''
-
 if __name__ == '__main__':
     app.run(debug=True)

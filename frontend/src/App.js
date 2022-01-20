f9bf99715bbc534bf2b9f765300c47c0599b2c7c
Haritha Divakaran <haritha.divakar@gmail.com>
2022-01-20 11:55:15 +0800

    import theme for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./components/HomePage";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>Hello</div>
 
       <HomePage />
+      <Login />
     </>
   );
 }

65439fd269b78da6e3c65a4330ceaef0935b55e2
samhita <1814-samhita@users.noreply.git.sysop.bigo.sg>
2022-01-20 15:41:43 +0800

    static homepage

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import "./bootstrap.min.css";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <div>Hello</div>
-      <Link to="/homepage">Homepage</Link>
+      <Link to="/HomePage">HomePage</Link>
       <Outlet />
     </>
   );
